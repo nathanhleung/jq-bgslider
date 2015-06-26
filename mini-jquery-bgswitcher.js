7699aaab00b7176f1a26bc57649450e213fb6453
Leung Enterprises <LeungEnterprises@users.noreply.github.com>
2015-06-25 23:29:20 -0400

    fix classname, add defaults

--- mini-jquery-bgswitcher.js
+++ mini-jquery-bgswitcher.js
@@ -1,11 +1,18 @@
 var bgSlider = function(params) {
-    if (!params) {
-        var params = {
-            images: ['http://i.imgur.com/gz0Yq3E.jpg', 'http://i.imgur.com/e3Fjb93.jpg', 'http://i.imgur.com/8FoJQYg.jpg'],
-            el: 'body',
-            animDuration: 1000,
-            slideDuration: 5000
-        };
+    if (typeof params === "undefined") {
+        params = {};
+    }
+    if (typeof params.images === "undefined") {
+        params.images = ['http://i.imgur.com/gz0Yq3E.jpg', 'http://i.imgur.com/e3Fjb93.jpg', 'http://i.imgur.com/8FoJQYg.jpg'];
+    }
+    if (typeof params.el === "undefined") {
+        params.el = 'body';
+    }
+    if (typeof params.animDuration === "undefined") {
+        params.animDuration = 1000;
+    }
+    if (typeof params.slideDuration === "undefined") {
+        params.slideDuration = 5000;
     }
     if (params.slideDuration < params.animDuration * 1.5) {
         jQuery.error('The slide duration must be at least 50% longer than the animation duration.');
@@ -15,13 +22,13 @@ var bgSlider = function(params) {
     }
     var images = params.images;
     for (var image in images) {
-       $(params.el).append('<div class="bg", id="img' + image + '"></div>');
+       $(params.el).append('<div class="bg-slider-img", id="img' + image + '"></div>');
        $('#img' + image).css('background-image', 'url("' + images[image] + '")');
        $('#img' + image).css('left',  String(-100 * image) + '%');
     }
     var counter = 0;
     var changePic = function() {
-        $('.bg').animate({
+        $('.bg-slider-img').animate({
             left: '+=100%'
         }, params.animDuration, function() {
             if ((counter - (images.length - 1)) % (images.length) === 0) {
